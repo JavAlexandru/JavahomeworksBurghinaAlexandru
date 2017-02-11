a8cd55f730c06910061206d2ef2372dee5043152
Alex <Alex@DESKTOP-I4HKI4T>
2017-02-11 05:00:51 +0200

    My first java homework

---
+++ Home1/Homework2/src/homework2/Homework2.java
@@ -0,0 +1,19 @@
+/*
+ * Homework2
+ */
+package homework2;
+
+/**
+ *
+ * @author Alex
+ */
+public class Homework2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is my first java homework");
+    }
+    
+}
